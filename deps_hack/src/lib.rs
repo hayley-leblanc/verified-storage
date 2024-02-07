b8db9c864f6942e0f4d1a0af7c0943ab5be809d2
hayley-leblanc <hleblanc@utexas.edu>
2024-02-07 11:07:35 -0600

    Cargo.toml and conditional compilation updates

--- deps_hack/src/lib.rs
+++ deps_hack/src/lib.rs
@@ -2,9 +2,18 @@ pub use core;
 pub use crc64fast;
 pub use rand;
 pub use winapi;
+#[cfg(windows)]
 pub use winapi::shared::winerror::SUCCEEDED;
+#[cfg(windows)]
 pub use winapi::um::fileapi::{CreateFileA, OPEN_ALWAYS};
+#[cfg(windows)]
 pub use winapi::um::handleapi::INVALID_HANDLE_VALUE;
+#[cfg(windows)]
 pub use winapi::um::memoryapi::{MapViewOfFile, FILE_MAP_ALL_ACCESS};
+#[cfg(windows)]
 pub use winapi::um::winbase::CreateFileMappingA;
-pub use winapi::um::winnt::{PAGE_READWRITE, FILE_SHARE_WRITE, FILE_SHARE_READ, FILE_SHARE_DELETE, GENERIC_READ, GENERIC_WRITE, FILE_ATTRIBUTE_NORMAL, ULARGE_INTEGER};
+#[cfg(windows)]
+pub use winapi::um::winnt::{
+    FILE_ATTRIBUTE_NORMAL, FILE_SHARE_DELETE, FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ,
+    GENERIC_WRITE, PAGE_READWRITE, ULARGE_INTEGER,
+};
